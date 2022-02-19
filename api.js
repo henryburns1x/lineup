521857a513ca9889bca74ab289d4cfd04d4be8ee
BluePiston <31391551+BluePiston@users.noreply.github.com>
2022-02-19 16:11:35 -0700

    hopefullythis won't break

--- api.js
+++ api.js
@@ -1,9 +1,16 @@
+//require('dotenv').config()
+
 const express = require('express');
 const config = require('./config.js');
 
 const mongoose = require('mongoose');
+//mongoose.connect(process.env.MONGODB_URI);
 
 const router = express.Router();
+
+const port = process.env.PORT || 2000;
+
+app.listen(port);
 
 router.get('/user', (request, response) => {
 
